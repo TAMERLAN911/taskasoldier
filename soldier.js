4e09003018b3754de3a179ed101b4a2071c18646
Tamerlan <tamer_idrisov98@mail.ru>
2022-05-18 14:22:25 +0300

    task.1.2

--- soldier.js
+++ soldier.js
@@ -1,17 +1,30 @@
 let soldier = {
-    name: 'ben',
-    health: 10 ,
-    weapon: {
-        wname: 'deagle'
-        amo: 30
-    },
-    fire: function () {
-
-    },
-    recharge: function () {
-
-    },
-    hurt: function () {
-
+  name: "ben",
+  health: 10,
+  weapon: {
+    wname: "deagle",
+    amo: 30,
+  },
+  supplies: 8,
+  fire: function () {
+    --this.amo;
+    if (this.amo < 1) {
+      return "обойма пуста перезаредитесь";
     }
-}
+    return "бах-бах";
+  },
+  recharge: function () {
+    this.amo = 30;
+    --this.supplies;
+    if (this.supplies < 1) {
+      return "не осталось припасов";
+    }
+    return "перезарядка";
+  },
+  hurt: function () {
+    --this.health;
+    if (this.health < 1) {
+      return "ты проиграл";
+    }
+  },
+};
