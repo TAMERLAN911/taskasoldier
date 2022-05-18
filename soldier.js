64a1cad38401f7b408fb3c17a5dda931966feb96
Tamerlan <tamer_idrisov98@mail.ru>
2022-05-18 11:15:01 +0300

    tamer

---
+++ soldier.js
@@ -0,0 +1,17 @@
+let soldier = {
+    name: 'ben',
+    health: 10 ,
+    weapon: {
+        wname: 'deagle'
+        amo: 30
+    },
+    fire: function () {
+
+    },
+    recharge: function () {
+
+    },
+    hurt: function () {
+
+    }
+}
